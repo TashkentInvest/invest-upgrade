c960b0fbf28433c73552a6660704f8f3496fd58c
atwebuz <abdusattorov2004@mail.ru>
2024-05-26 19:23:12 +0500

    js updated

---
+++ resources/views/pages/products/index.js
@@ -0,0 +1,37 @@
+let companyKubmetr = 1
+let minimumWage = 340000
+let generatePrice = companyKubmetr * minimumWage;
+
+let percentageInput = 20
+let quarterlyInput = 6
+
+
+if (percentageInput) {
+    let z = (generatePrice * percentageInput) / 100;
+    let n = generatePrice - z;
+
+    if (quarterlyInput) {
+        let y = n / quarterlyInput;
+        console.log(y)
+        console.log('kopaygan 6: ',y * 6)
+        console.log('qoshilgam 6: ',y )
+
+                
+        x = Math.floor(y)
+        
+        console.log('x: ', x * 6)
+    }
+}
+
+
+
+console.log(1000 / 6)
+
+console.log('qoshlgan: ',166.66666666666666 + 166.66666666666666 + 166.66666666666666 + 166.66666666666666 + 166.66666666666666 + 166.66666666666666)
+console.log('kopaygan: ',166.66666666666666 * 6)
+
+
+
+// let x = 4.59;
+// let z = Math.floor(x);
+// console.log("Converted value of " + x + " is " + z);
---
+++ resources/views/pages/products/main.js
@@ -0,0 +1,14 @@
+let a = 340000
+let b = (a * 20) / 100;
+console.log('b: ',b)
+let c  = a - b
+console.log('c: ',c)
+let n = 6 // kvartl
+let e = c / n
+console.log('e: ',e)
+let k = e * (n-1)
+console.log('k: ',k)
+let d = c - k
+console.log('d: ',d)
+let x = k + d
+console.log('x: ',x)
