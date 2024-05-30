3356c5e4da7d802b3dd8b5b113e3da1c0cfab359
atwebuz <abdusattorov2004@mail.ru>
2024-05-30 14:57:16 +0500

    working with exel;

--- app/Exports/ProductsExport.php
+++ app/Exports/ProductsExport.php
@@ -2,106 +2,86 @@
 
 namespace App\Exports;
 
-use App\Models\Client;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
-use Maatwebsite\Excel\Facades\Excel;
 
 class ProductsExport implements FromCollection, WithHeadings, WithColumnFormatting, ShouldAutoSize
 {
-    protected $id;
-
-    public function __construct($id)
-    {
-        $this->id = $id;
-    }
-
     public function collection()
     {
-        // Manual join to fetch data from clients, companies, and branches tables
-        $data = DB::table('clients')
-            ->join('companies', 'clients.id', '=', 'companies.client_id')
-            ->join('branches', 'companies.id', '=', 'branches.company_id')
-            ->select(
-                'clients.id',
-                'clients.first_name',
-                'clients.last_name',
-                'clients.father_name',
-                'clients.first_name as client_name',
-                'companies.company_name',
-                'companies.company_location',
-                'companies.raxbar',
-                'branches.contract_apt',
-                'branches.contract_date',
-                'branches.branch_kubmetr',
-                'branches.generate_price',
-                'branches.payment_type',
-                'branches.percentage_input',
-                'branches.installment_quarterly'
-            )
-            ->where('clients.id', $this->id)
-            ->get();
+        // Manual join to fetch data from your tables
+        $data = collect([
+            [
+                '№' => 1,
+                'Номер заявления' => '117688604',
+                'Наименование организации' => '"BUILDING CONNECTION" МЧЖ',
+                'Контакты' => 'Шохруз +998901979696',
+                'Район' => 'Алмазарский',
+                'Расчетный объем здания' => '33 818,40',
+                'Инфраструктурный платеж (сўм) по договору' => '11 498 256 000',
+                'Первый платеж (сум) 20% от стоимости' => '2 299 651 200',
+                'оплаченная сумма (сўм)' => '2,299,651,200.00',
+                'Дата оплаты' => '5/16/2024',
+                '№ договора' => 'АРТ-1/24',
+                'Дата договора' => '4/8/2024',
+                '№ уведомления' => '',
+                'Дата увед' => '',
+                'Страховой полис' => '',
+                'Банковская гарантия' => '',
+                'Примечание' => 'получил договор, оплатил 20% в полном объеме 16.05.2024'
+            ],
+            // Add the rest of your data here...
+        ]);
 
         return $data;
     }
 
     public function headings(): array
     {
-        // Define the column headings
         return [
-            'ID',
-            'First Name',
-            'Last Name',
-            'Father Name',
-            'Client Name',
-            'Company Name',
-            'Company Location',
-            'Company Raxbar',
-            'Contract Apt',
-            'Contract Date',
-            'Branch Kubmetr',
-            'Generate Price',
-            'Payment Type',
-            'Percentage Input',
-            'Installment Quarterly'
+            '№',
+            'Номер заявления',
+            'Наименование организации',
+            'Контакты',
+            'Район',
+            'Расчетный объем здания',
+            'Инфраструктурный платеж (сўм) по договору',
+            'Первый платеж (сум) 20% от стоимости',
+            'оплаченная сумма (сўм)',
+            'Дата оплаты',
+            '№ договора',
+            'Дата договора',
+            '№ уведомления',
+            'Дата увед',
+            'Страховой полис',
+            'Банковская гарантия',
+            'Примечание'
         ];
     }
 
     public function columnFormats(): array
     {
-        // Define column widths
         return [
-            'A' => 10,
-            'B' => 20,
-            'C' => 20,
-            'D' => 20,
-            'E' => 20,
-            'F' => 20,
-            'G' => 20,
-            'H' => 20,
-            'I' => 20,
-            'J' => 20,
-            'K' => 20,
-            'L' => 20,
-            'M' => 20,
-            'N' => 20,
-            'O' => 20,
+            'A' => '@',
+            'B' => '@',
+            'C' => '@',
+            'D' => '@',
+            'E' => '@',
+            'F' => '@',
+            'G' => '@',
+            'H' => '@',
+            'I' => '@',
+            'J' => '@',
+            'K' => '@',
+            'L' => '@',
+            'M' => '@',
+            'N' => '@',
+            'O' => '@',
+            'P' => '@',
+            'Q' => '@'
         ];
-    }
-
-    public static function downloadTableData($id)
-    {
-        // Retrieve client details
-        $client = Client::find($id);
-
-        // Generate file name with current date, client's first name, and last name
-        $fileName = 'products_data_' . $client->first_name . '_' . $client->last_name . '_' . now()->format('Y-m-d') . '.xls';
-
-        // Download Excel file with dynamically generated file name
-        return Excel::download(new ProductsExport($id), $fileName);
     }
 }
