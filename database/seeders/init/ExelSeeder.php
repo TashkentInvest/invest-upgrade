5c16ee9f04d16fb38b5f713239c3f552b4e07eac
atwebuz <abdusattorov2004@mail.ru>
2024-05-30 22:50:20 +0500

    seeding timestamp was fixed

--- database/seeders/init/ExelSeeder.php
+++ database/seeders/init/ExelSeeder.php
@@ -1,12 +1,10 @@
 <?php
-
 namespace Database\Seeders\init;
 
 use App\Models\Branch;
 use App\Models\Client;
 use App\Models\Company;
 use App\Models\Products;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Regions;
 
@@ -19,45 +17,55 @@ class ExelSeeder extends Seeder
      */
     public function run()
     {
-        $exelData = json_decode(file_get_contents(__DIR__."/../references/exel.json"));
-        echo "Exel::inserting".PHP_EOL;
+        $exelData = json_decode(file_get_contents(__DIR__ . "/../references/exel.json"));
+        echo "Exel::inserting" . PHP_EOL;
+        
         foreach ($exelData as $item) {
             $clientData = [
                 'application_number' => $item->application_number ?? null,
                 'contact' => $item->contact ?? null,
                 'client_description' => isset($item->client_description) ? $item->client_description : null,
             ];
-        
+
             $client = Client::create($clientData);
-        
+
             $companyData = [
                 'client_id' => $client->id,
                 'company_name' => $item->company_name ?? null,
             ];
-        
+
             $company = Company::create($companyData);
-        
+
+            // Check if payed_date is a valid date
+            $payed_date = null;
+            if (isset($item->payed_date)) {
+                try {
+                    $payed_date = new \DateTime($item->payed_date);
+                    $payed_date = $payed_date->format('Y-m-d H:i:s');
+                } catch (\Exception $e) {
+                    $payed_date = null;
+                }
+            }
+
             $branchData = [
                 'company_id' => $company->id ?? null,
-                // 'location' => $item->branch_location ?? null,
                 'branch_kubmetr' => $item->branch_kubmetr ?? null,
                 'generate_price' => $item->generate_price ?? null,
                 'first_payment_percent' => $item->first_payment_percent ?? null,
                 'payed_sum' => isset($item->payed_sum) ? $item->payed_sum : null,
-                'payed_date' => isset($item->payed_date) ? $item->payed_date : null,
+                'payed_date' => $payed_date,
                 'contract_apt' => $item->contract_apt ?? null,
             ];
-        
+
             $branch = Branch::create($branchData);
-        
+
             $productData = [
                 'client_id' => $client->id,
                 'user_id' => 1,
                 'minimum_wage' => $item->minimum_wage ?? null,
             ];
-        
+
             $product = Products::create($productData);
         }
-        
     }
 }
