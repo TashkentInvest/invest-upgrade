bba9ce97d6ec032110e0e232f9360f72089f7c45
atwebuz <abdusattorov2004@mail.ru>
2024-06-03 18:15:48 +0500

    starting to create artisan command

--- app/Http/Controllers/AuditLogController.php
+++ app/Http/Controllers/AuditLogController.php
@@ -4,31 +4,23 @@ namespace App\Http\Controllers;
 
 use App\Models\AuditLog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 
 class AuditLogController extends Controller
 {
-    // public function index()
-    // {
-    //     try {
-    //         $auditLogs = AuditLog::with(['user', 'client', 'company'])
-    //                              ->orderBy('created_at', 'desc')
-    //                              ->get();
-    //     } catch (\Exception $e) {
-    //         return back()->withError($e->getMessage())->withInput();
-    //     }
-    
-    //     return view('pages.audit-logs.index', compact('auditLogs'));
-    // }
-    
     public function index()
     {
-        $auditLogs = AuditLog::with(['user', 'client'])->orderBy('created_at', 'desc')->get();
+
+        $auditLogs = Cache::remember('auditLogs', 30 * 60, function () {
+            return AuditLog::with(['user', 'client'])->orderBy('created_at', 'desc')->get();
+        });
+        
         return view('pages.audit-logs.index', compact('auditLogs'));
     }
-    // public function index()
-    // {
-    //     $auditLogs = AuditLog::with(['user', 'client'])->orderBy('created_at', 'desc')->get();
-    //     return view('pages.audit-logs.index', compact('auditLogs'));
-    // }
+
+    //     We want to cache the result for 30 minutes.
+    // 1 minute has 60 seconds, so 30 minutes would be 30 * 60 seconds.
+    // Therefore, we set the cache to expire after 30 * 60 seconds, which equals 1800 seconds.
     
 }
