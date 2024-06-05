4f44f89cea4beffa12ae48345d6ff09fbea40357
atwebuz <abdusattorov2004@mail.ru>
2024-06-05 14:46:52 +0500

    word was fixed

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -20,7 +20,53 @@ class FileController extends Controller
         return view('pages.docs.fizik_litso', compact('client'));
     }
     // download word
+
     public function show($id)
+    {
+        $client = Client::with('products')->with('branches')->where('is_deleted', '!=', 1)->find($id);
+        $client->yuridik_rekvizid;
+        $client->contact;
+        $client->company_type;
+        $client->company_location;
+        $client->bank_code;
+        $client->stir;
+        $client->oked;
+        $client->company_location;
+
+
+        $branchDocuments = []; 
+
+          
+            foreach ($client->branches as $branch) {
+                $branch->generate_price;
+                // $branch->payment_type;
+                $branch->branch_kubmetr;
+
+                // Generate document for each branch and store it in the array
+                $headers = [
+                    'Content-type' => 'text/html',
+                    'Content-Disposition' => 'attachment; Filename=' . $client->company_name . '_branch_' . $branch->id . '.doc'
+                ];
+
+                $branchDocument = view('pages.docs.bolib_pay.fizik_litso', compact('client', 'branch'))->render();
+                $branchDocuments[] = ['document' => $branchDocument, 'headers' => $headers];
+            }
+        
+
+        // Zip the documents
+        $zip = new \ZipArchive();
+        $zipFileName = storage_path('app/АПЗ_' . Carbon::now()->format('Y-m-d') .  '.zip');
+        if ($zip->open($zipFileName, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) === true) {
+            foreach ($branchDocuments as $branchDoc) {
+                $zip->addFromString(basename($branchDoc['headers']['Content-Disposition']), $branchDoc['document']);
+            }
+            $zip->close();
+        }
+
+        // Download the zip file
+        return response()->download($zipFileName)->deleteFileAfterSend(true);
+    }
+    public function show_org($id)
     {
         $client = Client::with('products')->with('companies')->where('is_deleted', '!=', 1)->find($id);
         $client->yuridik_rekvizid;
