150dad55fed74bdc46b419b78fbb4f6b5256edc3
atwebuz <abdusattorov2004@mail.ru>
2024-06-11 11:50:52 +0500

    some changes

--- resources/views/pages/backup/index.blade.php
+++ resources/views/pages/backup/index.blade.php
@@ -135,7 +135,6 @@
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
     <script>
         var totalSize = "{{ $totalSizeFormatted }}";
