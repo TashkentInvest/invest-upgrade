7fc1b6fe0845f00a3f8847002d6cb6277e9070fb
atwebuz <abdusattorov2004@mail.ru>
2024-06-11 12:41:39 +0500

    working with updating transaction blade

--- resources/views/pages/transactions/index.blade.test.php
+++ resources/views/pages/transactions/index.blade.test.php
@@ -36,7 +36,7 @@
                         <select class="form-control select2">
                             <option>Status</option>
                             <option value="Active">Active</option>
-                            <option value="New">New</</option>
+                            <option value="New">New</option>
                             <option value="Close">Close</option>
                         </select>
                     </div>
@@ -59,34 +59,49 @@
             </div>
             <div class="card-body">
                 <div class="table-responsive">
-                    <table class="table table-bordered table-hover align-middle nowrap">
-                        <thead class="table-light">
-                            <tr>
-                                <th scope="col">No</th>
-                                <th scope="col">№ Док</th>
-                                <th scope="col">Дата платежа</th>
-                                <th scope="col" style="width: 200px;">Назначение платежа</th>
-                                <th scope="col">Дебит</th>
-                                <th scope="col">Кредит</th>
-                                <th scope="col">Инн</th>
-                                <th scope="col">МФО</th>
-                                <th scope="col">Расчетный счет</th>
-                                <th scope="col">Actions</th>
-                            </tr>
+                    <table class="table table-bordered align-middle nowrap">
+                        <thead>
+                            <th scope="col">No</th>
+                                    <th scope="col">№ Док</th>
+                                    {{-- <th scope="col">Operation Code</th>
+                                    <th scope="col">Recipient Name</th>
+                                    <th scope="col">Recipient INN</th>
+                                    <th scope="col">Recipient MFO</th>
+                                    <th scope="col">Recipient Account</th> --}}
+                                    <th scope="col">Дата платежа</th>
+                                    <th scope="col">Назначение платежа</th>
+                                    <th scope="col">Дебит</th>
+                                    <th scope="col">Кредит</th>
+                                    {{-- <th scope="col">Payer Name</th> --}}
+                                    <th scope="col">Инн</th>
+                                    <th scope="col">МФО</th>
+                                    {{-- <th scope="col">Payer Bank</th> --}}
+                                    <th scope="col">Расчетный счет</th>
+                                   
                         </thead>
                         <tbody>
                             @foreach ($transactions as $transaction)
+                            {{-- @dd($transaction->document_number) --}}
                                 <tr>
                                     <td>{{ $transaction->id }}</td>
                                     <td>{{ $transaction->document_number }}</td>
+                                    {{-- <td>{{ $transaction->operation_code }}</td>
+                                    <td>{{ $transaction->recipient_name }}</td>
+                                    <td>{{ $transaction->recipient_inn }}</td>
+                                    <td>{{ $transaction->recipient_mfo }}</td>
+                                    <td>{{ $transaction->recipient_account }}</td> --}}
                                     <td>{{ $transaction->payment_date }}</td>
-                                    <td style="width: 200px;">{{ $transaction->payment_description }}</td>
+                                    <td>{{ $transaction->payment_description }}</td>
                                     <td>{{ $transaction->debit }}</td>
                                     <td>{{ $transaction->credit }}</td>
+                                    {{-- <td>{{ $transaction->payer_name }}</td> --}}
                                     <td>{{ $transaction->payer_inn }}</td>
                                     <td>{{ $transaction->payer_mfo }}</td>
+                                    {{-- <td>{{ $transaction->payer_bank }}</td> --}}
                                     <td>{{ $transaction->payer_account }}</td>
+
                                     <td>
+                                        
                                         <ul class="list-unstyled hstack gap-1 mb-0">
                                             <li data-bs-toggle="tooltip" data-bs-placement="top" aria-label="View">
                                                 <a href="{{ route('transactions.show', $transaction->id) }}" class="btn btn-sm btn-soft-primary"><i class="mdi mdi-eye-outline"></i></a>
@@ -104,28 +119,3 @@
     </div><!--end col-->
 </div><!--end row-->
 @endsection
-
-@push('styles')
-<style>
-    .table th, .table td {
-        vertical-align: middle;
-        text-align: center;
-    }
-    .table th {
-        background-color: #f8f9fa;
-    }
-    .btn-soft-primary {
-        background-color: #d1e7dd;
-        border-color: #badbcc;
-        color: #0f5132;
-    }
-    .btn-soft-primary:hover {
-        background-color: #badbcc;
-        border-color: #a5cfbb;
-        color: #0f5132;
-    }
-    .card-body.border-bottom {
-        border-bottom: 1px solid #e9ecef !important;
-    }
-</style>
-@endpush
