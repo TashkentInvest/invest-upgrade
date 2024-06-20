e44b99c0c02a7f4622763426eba04786a057a744
atwebuz <abdusattorov2004@mail.ru>
2024-06-20 18:23:21 +0500

    starting to add beauty

--- resources/views/pages/construction/chat/index.blade.php
+++ resources/views/pages/construction/chat/index.blade.php
@@ -70,6 +70,32 @@
                                     </div>
                                 </div>
                             </li>
+
+                            {{-- <li>
+                                <div class="conversation-list">
+                                    <div class="dropdown">
+
+                                        <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                          </a>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="#">Copy</a>
+                                            <a class="dropdown-item" href="#">Save</a>
+                                            <a class="dropdown-item" href="#">Forward</a>
+                                            <a class="dropdown-item" href="#">Delete</a>
+                                        </div>
+                                    </div>
+                                    <div class="ctext-wrap">
+                                        <div class="conversation-name">Steven Franklin</div>
+                                        <p>
+                                            Yeah everything is fine
+                                        </p>
+                                        
+                                        <p class="chat-time mb-0"><i class="bx bx-time-five align-middle me-1"></i> 10:06</p>
+                                    </div>
+                                    
+                                </div>
+                            </li> --}}
                         @endforeach
                     </ul>
                 </div>
