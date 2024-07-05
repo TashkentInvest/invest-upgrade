fe2ca7e029a6f0ae6642e3ac266d48c8bcf67a12
atwebuz <abdusattorov2004@mail.ru>
2024-07-05 12:43:28 +0500

    starting to add new migrations

---
+++ database/migrations/2024_07_05_111925_add_new_columns_to_branches_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewColumnsToBranchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->string('shaxarsozlik_umumiy_xajmi')->nullable();
+            $table->string('qavatlar_soni_xajmi')->nullable();
+            $table->string('avtoturargoh_xajmi')->nullable();
+            $table->string('qavat_xona_xajmi')->nullable();
+            $table->string('umumiy_foydalanishdagi_xajmi')->nullable();
+            $table->text('qurilish_turi')->nullable();
+            $table->decimal('coefficient', 8, 2)->nullable();
+            $table->string('zona')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('branches', function (Blueprint $table) {
+            $table->dropColumn('shaxarsozlik_umumiy_xajmi');
+            $table->dropColumn('qavatlar_soni_xajmi');
+            $table->dropColumn('avtoturargoh_xajmi');
+            $table->dropColumn('qavat_xona_xajmi');
+            $table->dropColumn('umumiy_foydalanishdagi_xajmi');
+            $table->dropColumn('qurilish_turi');
+            $table->dropColumn('coefficient');
+            $table->dropColumn('zona');
+        });
+    }
+}
