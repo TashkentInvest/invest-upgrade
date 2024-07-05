b50f94a029ac0bc60481adf4fe34034a5ea50140
atwebuz <abdusattorov2004@mail.ru>
2024-07-05 11:17:58 +0500

    starting to add new values

--- database/migrations/2024_05_24_145439_create_clients_table.php
+++ database/migrations/2024_05_24_145439_create_clients_table.php
@@ -13,38 +13,6 @@ class CreateClientsTable extends Migration
      */
     public function up()
     {
-        // Schema::create('clients', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->enum('mijoz_turi', ['yuridik', 'fizik'])->nullable();
-        //     $table->string('first_name')->nullable();
-        //     $table->string('last_name')->nullable();
-        //     $table->string('father_name')->nullable();
-        //     $table->string('contact')->nullable();
-        //     $table->string('yuridik_address')->nullable();
-        //     $table->string('home_address')->nullable();
-        //     $table->string('yuridik_rekvizid')->nullable();
-        //     $table->string('passport_serial')->nullable();
-        //     $table->string('passport_pinfl')->nullable();
-        //     $table->dateTime('passport_date')->nullable();
-        //     $table->string('passport_location')->nullable();
-        //     $table->boolean('passport_type')->nullable()->default(0);
-        //     $table->boolean('is_deleted')->nullable()->default(0);
-        //     $table->string('company_location')->nullable();
-        //     $table->string('company_name')->nullable();
-        //     $table->string('raxbar')->nullable();
-        //     $table->string('bank_code')->nullable();
-        //     $table->string('bank_service')->nullable();
-        //     $table->string('bank_account')->nullable();
-        //     $table->string('stir')->nullable();
-        //     $table->string('oked')->nullable();
-        //     $table->text('minimum_wage')->nullable();
-        //     $table->integer('status')->default(1);
-        //     $table->timestamps();
-
-        //     $table->index('is_deleted');
-        //     $table->index(['last_name', 'first_name']);
-        // });
-
         Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id')->nullable();
@@ -63,7 +31,6 @@ class CreateClientsTable extends Migration
             $table->index(['last_name', 'first_name']);
         });
     }
-    
     
     /**
      * Reverse the migrations.
