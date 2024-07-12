fe6c14d59dd7f4fbb81cd8bf0f6e5feb662865db
atwebuz <abdusattorov2004@mail.ru>
2024-07-12 15:35:02 +0500

    num to text was fixed

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -15,82 +15,6 @@ use Carbon\Carbon;
 
 class FileController extends Controller
 {
-    public function show($id)
-    {
-        // Fetch the client along with branches if not deleted
-        $client = Client::with('branches')
-            ->where('is_deleted', '!=', 1)
-            ->find($id);
-
-        if (!$client) {
-            return response()->view('errors.custom', ['status' => 404, 'message' => 'Client Not Found'], 404);
-        }
-
-        $clientAttributes = [
-            'yuridik_rekvizid',
-            'contact',
-            'branch_type',
-            'company_location',
-            'bank_code',
-            'stir',
-            'oked'
-        ];
-
-        foreach ($clientAttributes as $attribute) {
-            $client->$attribute;
-        }
-
-        $branchDocuments = [];
-
-        // Determine the views based on the client type and payment type
-        $fizikBolibView = 'pages.docs.bolib_pay.fizik_litso';
-        $fizikFullView = 'pages.docs.full_pay.fizik_litso';
-        $yurikBolibView = 'pages.docs.bolib_pay.yurik_litso';
-        $yurikFullView = 'pages.docs.full_pay.yurik_litso';
-
-        // $passport_formattedDate = Carbon::parse($client->passport_date)->format('Y-m-d H:i:s');
-        // $client->passport_serial;
-        // $client->raxbar;
-        // dd($client->passport_serial);
-
-        foreach ($client->branches as $branch) {
-            $branch->generate_price;
-            $branch->branch_kubmetr;
-            $branch->branch_type;
-            $branch->branch_location;
-
-            // Ensure valid filenames with identifiers
-            $fizikBolibFilename = 'fizik_bolib_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
-            $fizikFullFilename = 'fizik_full_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
-            $yurikBolibFilename = 'yurik_bolib_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
-            $yurikFullFilename = 'yurik_full_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
-
-            // Render documents for each branch and add them to the branchDocuments array
-            $branchDocuments[] = ['document' => view($fizikBolibView, compact('client', 'branch'))->render(), 'filename' => $fizikBolibFilename];
-            $branchDocuments[] = ['document' => view($fizikFullView, compact('client', 'branch'))->render(), 'filename' => $fizikFullFilename];
-            $branchDocuments[] = ['document' => view($yurikBolibView, compact('client', 'branch'))->render(), 'filename' => $yurikBolibFilename];
-            $branchDocuments[] = ['document' => view($yurikFullView, compact('client', 'branch'))->render(), 'filename' => $yurikFullFilename];
-        }
-
-
-        $zip = new \ZipArchive();
-        $zipFileName = storage_path('app/АПЗ_' . \Carbon\Carbon::now()->format('Y-m-d') . '.zip');
-
-        if ($zip->open($zipFileName, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) !== true) {
-            // Log error if zip file cannot be opened
-            \Log::error('Cannot create zip file at ' . $zipFileName);
-            return response()->view('errors.custom', ['status' => 500, 'message' => 'Cannot create zip file'], 500);
-        }
-
-        foreach ($branchDocuments as $branchDoc) {
-            $zip->addFromString($branchDoc['filename'], $branchDoc['document']);
-        }
-        $zip->close();
-
-        return response()->download($zipFileName)->deleteFileAfterSend(true);
-    }
-
-    
 
     public function downloadFullTableData($startDate = null, $endDate = null)
     {
@@ -154,5 +78,211 @@ class FileController extends Controller
     
     //     return view('pages.docs.mobile', compact('client'));
     // }
+
+    // Number to text start ---------------------------------------------------------------------------------
+
+
+    public function show($id)
+    {
+        // Fetch the client along with branches if not deleted
+        $client = Client::with('branches')
+            ->where('is_deleted', '!=', 1)
+            ->find($id);
+
+        if (!$client) {
+            return response()->view('errors.custom', ['status' => 404, 'message' => 'Client Not Found'], 404);
+        }
+
+        $clientAttributes = [
+            'yuridik_rekvizid',
+            'contact',
+            'branch_type',
+            'company_location',
+            'bank_code',
+            'stir',
+            'oked'
+        ];
+
+        foreach ($clientAttributes as $attribute) {
+            $client->$attribute;
+        }
+
+        $branchDocuments = [];
+
+        $fizikBolibView = 'pages.docs.bolib_pay.fizik_litso';
+        $fizikFullView = 'pages.docs.full_pay.fizik_litso';
+        $yurikBolibView = 'pages.docs.bolib_pay.yurik_litso';
+        $yurikFullView = 'pages.docs.full_pay.yurik_litso';
+
+        foreach ($client->branches as $branch) {
+            $branch->generate_price;
+            $branch->branch_kubmetr_text = $this->transformToText($branch->branch_kubmetr);
+            $branch->branch_kubmetr;
+            $branch->branch_type;
+            $branch->branch_location;
+
+            // Ensure valid filenames with identifiers
+            $fizikBolibFilename = 'fizik_bolib_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
+            $fizikFullFilename = 'fizik_full_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
+            $yurikBolibFilename = 'yurik_bolib_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
+            $yurikFullFilename = 'yurik_full_' . preg_replace('/[<>:"\/\\|?*]+/', '_', $client->company_name . '_branch_' . $branch->id . '_name_' . $branch->contract_apt . '.doc');
+
+            // Render documents for each branch and add them to the branchDocuments array
+            $branchDocuments[] = ['document' => view($fizikBolibView, compact('client', 'branch'))->render(), 'filename' => $fizikBolibFilename];
+            $branchDocuments[] = ['document' => view($fizikFullView, compact('client', 'branch'))->render(), 'filename' => $fizikFullFilename];
+            $branchDocuments[] = ['document' => view($yurikBolibView, compact('client', 'branch'))->render(), 'filename' => $yurikBolibFilename];
+            $branchDocuments[] = ['document' => view($yurikFullView, compact('client', 'branch'))->render(), 'filename' => $yurikFullFilename];
+        }
+
+
+        $zip = new \ZipArchive();
+        $zipFileName = storage_path('app/АПЗ_' . \Carbon\Carbon::now()->format('Y-m-d') . '.zip');
+
+        if ($zip->open($zipFileName, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) !== true) {
+            // Log error if zip file cannot be opened
+            \Log::error('Cannot create zip file at ' . $zipFileName);
+            return response()->view('errors.custom', ['status' => 500, 'message' => 'Cannot create zip file'], 500);
+        }
+
+        foreach ($branchDocuments as $branchDoc) {
+            $zip->addFromString($branchDoc['filename'], $branchDoc['document']);
+        }
+        $zip->close();
+
+        return response()->download($zipFileName)->deleteFileAfterSend(true);
+    }
+
+    private $units = array("бир", "икки", "уч", "тўрт", "беш", "олти", "етти", "саккиз", "тўққиз");
+    private $teens = array("ўн", "ўн бир", "ўн икки", "ўн уч", "ўн тўрт", "ўн беш",
+                           "ўн олти", "ўн етти", "ўн саккиз", "ўн тўққиз");
+    private $tens = array("йигирма", "ўттиз", "қирқ", "эллик", "олтмиш", "етмиш", "саксон", "тўқсон");
+    private $hundreds = array("юз", "икки юз", "уч юз", "тўрт юз", "беш юз", "олти юз", "етти юз", "саккиз юз", "тўққиз юз");
+    private $thousands = array("минг", "минг", "минг");
+    private $millions = array("миллион", "миллион", "миллион");
+    private $milliards = array("миллиард", "миллиард", "миллиард");
+    private $femailThousands = array("бир", "икки");
+    
+
+    public function convert(Request $request)
+    {
+        $value = $request->input('value');
+        if ($this->isValidValue($value)) {
+            $text = $this->transformToText($value);
+        } else {
+            $text = "Киритилган қиймат нотўғри ёки рухсат этилган диапазонга кирмайди";
+        }
+
+        return view('pages.number_to_text', compact('text'));
+    }
+
+    private function isValidValue($value)
+    {
+        if ($value == '0' || (preg_match("/^-{0,1}[1-9]{1,1}[0-9]{1,9}$/", $value, $matcher) && abs($matcher[0]) <= 2147483647)) {
+            return true;
+        }
+        return false;
+    }
+
+    private function transformToText($value)
+    {
+        $tempValue = $this->prepareValue($value);
+        if ($tempValue[1] == 0) {
+            return "нол";
+        }
+        $text = "";
+        $value = $tempValue[1];
+
+        if ($value >= 1000000000) {
+            $text .= $this->getTextByDigitClass($value, 1000000000);
+        }
+        if ($value >= 1000000) {
+            $text .= $this->getTextByDigitClass($value, 1000000);
+        }
+        if ($value >= 1000) {
+            $text .= $this->getTextByDigitClass($value, 1000);
+        }
+        if ($value >= 1) {
+            $text .= $this->getTextByDigitClass($value, 1);
+        }
+        if ($tempValue[0]) {
+            $text = $tempValue[0] . " " . $text;
+        }
+        return $text;
+    }
+
+    private function prepareValue($value)
+    {
+        $value = trim($value);
+        $value = str_replace(" ", "", $value);
+        $number = array();
+        if ($value[0] == '-') {
+            $number[] = 'minus';
+            $number[] = intval(substr($value, 1));
+        } else {
+            $number[] = '';
+            $number[] = intval(substr($value, 0));
+        }
+        return $number;
+    }
+
+    private function getTextByDigitClass($value, $digitClass)
+    {
+        $tempText = "";
+        $temp = $value / $digitClass;
+        $temp %= 1000;
+        $triade = $temp;
+        if ($triade >= 100) {
+            $tempText .= $this->hundreds[$triade / 100 - 1] . " ";
+            $triade %= 100;
+        }
+        if ($triade >= 10) {
+            if (intval($triade / 10) == 1) {
+                $tempText .= $this->teens[$triade - 10] . " ";
+            } else {
+                $tempText .= $this->tens[$triade / 10 - 2] . " ";
+                $triade %= 10;
+            }
+        }
+        if ($triade >= 1 && $triade < 10) {
+            if ($digitClass == 1000 && ($triade == 1 || $triade == 2)) {
+                $tempText .= $this->femailThousands[$triade - 1] . " ";
+            } else {
+                $tempText .= $this->units[$triade - 1] . " ";
+            }
+        }
+        if ($temp) {
+            switch ($digitClass) {
+                case 1000:
+                    $tempText .= $this->addDigitClassName($this->thousands, $triade);
+                    break;
+                case 1000000:
+                    $tempText .= $this->addDigitClassName($this->millions, $triade);
+                    break;
+                case 1000000000:
+                    $tempText .= $this->addDigitClassName($this->milliards, $triade);
+                    break;
+            }
+        }
+        return $tempText;
+    }
+
+    private function addDigitClassName($digitClassArr, $lastNumber)
+    {
+        $tempText = "";
+        switch ($lastNumber) {
+            case 1:
+                $tempText .= $digitClassArr[0] . " ";
+                break;
+            case 2:
+            case 3:
+            case 4:
+                $tempText .= $digitClassArr[1] . " ";
+                break;
+            default:
+                $tempText .= $digitClassArr[2] . " ";
+                break;
+        }
+        return $tempText;
+    }
 
 }
