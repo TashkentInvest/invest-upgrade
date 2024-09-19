31c865208760d45908adebdef9148fa48a971ad2
InvestUz <invest.abdusattorov@gmail.com>
2024-09-19 17:07:37 +0500

    upgrading

--- app/Models/Passport.php
+++ app/Models/Passport.php
@@ -7,15 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 use Venturecraft\Revisionable\RevisionableTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-
-
 class Passport extends Model
 {
     use HasFactory, SoftDeletes, RevisionableTrait;
+
     protected $fillable = ['client_id', 'passport_serial', 'passport_pinfl', 'passport_date', 'passport_location', 'passport_type'];
+
+    protected $casts = [
+        'passport_date' => 'date', // Ensure this has a comma at the end
+    ];
 
     public function client()
     {
         return $this->belongsTo(Client::class);
     }
 }
+
